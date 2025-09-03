f197fe63fa3c052595d7caf00031e4d95d6734ee
Ally Murray <allymurray88@gmail.com>
2025-09-03 10:22:14 +0100

    chore: :wrench: update site URL in Astro configuration to remove subdirectory

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://allymurray.github.io/wtec',
+  site: 'https://allymurray.github.io',
   base: '/',
 });
