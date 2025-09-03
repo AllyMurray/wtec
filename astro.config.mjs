0d115e1048c9b2adc4d36f5a4512ef1e4e88594e
Ally Murray <allymurray88@gmail.com>
2025-09-03 09:59:16 +0100

    chore: :wrench: update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://wtec.racing',
+  site: 'https://allymurray.github.io/wtec',
   base: '/',
 });
